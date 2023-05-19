476853018cd369cb0d02e01628a3da8211f648c0
Micah Njeru <micahnjeru1@gmail.com>
2023-05-19 11:10:33 +0300

    Added a favorites toggle button to filter favorite landmarks.

--- Landmarks/Landmarks/Model/ModelData.swift
+++ Landmarks/Landmarks/Model/ModelData.swift
@@ -5,9 +5,12 @@
 //  Created by Micah Jesse Njeru on 18/05/2023.
 //
 
+import Combine
 import Foundation
 
-var landmarks: [Landmark] = load("landmarkData.json")
+final class ModelData: ObservableObject {
+    @Published var landmarks: [Landmark] = load("landmarkData.json")
+}
 
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
