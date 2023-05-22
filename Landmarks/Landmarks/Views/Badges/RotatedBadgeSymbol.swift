e3042217dbb079a6095ca7a8ebd3d47464aa27e8
Micah Njeru <micahnjeru1@gmail.com>
2023-05-22 10:30:18 +0300

    Added a badge view folder and drew a badge

---
+++ Landmarks/Landmarks/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Micah Jesse Njeru on 22/05/2023.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
